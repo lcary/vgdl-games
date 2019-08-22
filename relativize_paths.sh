d01563fc7f82239977737d8a65f748fe1331e59f
Luc Cary <luc.cary@gmail.com>
2019-08-22 18:33:15 -0400

    relativize require paths

--- relativize_paths.sh
+++ relativize_paths.sh
@@ -4,3 +4,5 @@ set -e
 git grep -l 'href="/' -- games/ | xargs sed -i '' -e 's,href="/,href="../../,g'
 # relativize src paths
 git grep -l 'src="/' -- games/ | xargs sed -i '' -e 's,src="/,src="../../,g'
+# relativize require paths
+git grep -l "require.setModuleRoot('/')" -- games/ | xargs sed -i '' -e "s,require.setModuleRoot('/'),require.setModuleRoot('../..'),g"
