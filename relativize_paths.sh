e29391eb0a0c7d5779dad27ee857f86d16f3245f
Luc Cary <luc.cary@gmail.com>
2019-08-22 17:58:49 -0400

    fix source paths

--- relativize_paths.sh
+++ relativize_paths.sh
@@ -2,3 +2,5 @@
 set -e
 # relativize stylesheet paths
 git grep -l 'href="/' -- games/ | xargs sed -i '' -e 's,href="/,href="../../,g'
+# relativize src paths
+git grep -l 'src="/' -- games/ | xargs sed -i '' -e 's,src="/,src="../../,g'
